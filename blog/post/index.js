96f388118d314d6e3de8a0f2a78c2accd5a966f0
rgalicia0729 <rigo.galicia@icloud.com>
2021-03-08 16:06:08 -0600

    Iniciando con el servicio de posts

---
+++ blog/post/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id, title
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+
+app.listen(4000, (err) => {
+  if (err) console.error(err.message);
+
+  console.log('Post listen on the port 4000');
+});
